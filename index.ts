50cb4c7741fa942eb938b3b6d847a2f3b02029b0
avatarsik <avatarsik6699@gmail.com>
2020-09-10 15:49:52 +0700

    create class Queue for generates the numbers of the players who are putting a dot at the moment

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
+class Queue {
+    private count: number = null;
+    constructor(private amountPlayers: number) {}
+    getplayerNumber():number {
+        if (this.count === null) {
+            this.count = 0;
+        } else {
+            this.count++;
+            if (this.count >= this.amountPlayers) this.count = 0;
+        }
+        return this.count;
+    }
+}
+// класс точек
+class Dot {
 
-
+}
 // класс для взаимодействия с DOM элементами
 class HTML {
     constructor() {}
@@ -16,23 +31,32 @@ class HTML {
         anchor.append(table);
         return table;
     }
+    createDot():object { // возвращает DOM node точки
+        return new Dot;
+    }
 }
 
 class Field {
     private DOManchor: HTMLElement; // якорь для таблицы
-    private html: any = new HTML; // объект для работы с DOM
+    private html: HTML = new HTML; // объект для работы с DOM
     public field: HTMLElement; // таблица (игровое поле)
+    private queue: Queue = new Queue(2);
     constructor(anchor: string | HTMLElement, private colsNum:number, private rowsNum:number ) {
         this.DOManchor = typeof anchor === 'string' ? 
         document.querySelector(`.${anchor}`) 
         : anchor;
-        this.render(); //отрисовка таблицы
+        this.render(); // отрисовка таблицы
+        this.handle(); // навешываем обработчик на таблицу
     }
     private render():void {
         this.field = this.html.createField(this.DOManchor, this.colsNum, this.rowsNum);
     }
+    private handle():void {
+        this.field.addEventListener('click', (e) => {
+            console.log(this.queue.getplayerNumber())
+        })
+    }
 }
 
 let filed = new Field('game', 15, 15);
-console.log(filed.field);
 
